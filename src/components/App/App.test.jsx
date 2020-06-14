205016abd0f298c6ea9f5477189844b67e5acc21
kpodmasko <podmasko.konstantin@gmail.com>
2020-06-14 21:45:40 +0300

    fixed tests

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { shallow } from "enzyme";
 
-import App from "@components/App/index";
+import store from "@store";
+import App from "@components/App";
 
 describe("App", () => {
   it("renders without error", () => {
-    shallow(<App />);
+    shallow(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
   });
 });
