c01e2594ce9f76a2b836dcee3496561d930bb1fb
kpodmasko <podmasko.konstantin@gmail.com>
2020-06-14 20:56:20 +0300

    implemented Error

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,68 @@
+import React, { KeyboardEvent, MouseEvent, useCallback, FC, memo } from "react";
+import classNames from "classnames";
+
+import { IBaseProps } from "@declarations/interfaces";
+import { Classes } from "@declarations/types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
+import "./Error.scss";
+
+export const mainCssClass = "error";
+
+interface IErrorProps extends IBaseProps {
+  onClose?: () => void;
+}
+
+// TODO: add tests
+// TODO: think about beauty
+const Error: FC<IErrorProps> = (props: IErrorProps) => {
+  const { className, children, onClose } = props;
+
+  const classes: Classes = classNames(mainCssClass, className);
+
+  const handleCloseClick = useCallback(
+    (event: MouseEvent): void => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      if (onClose) {
+        onClose();
+      }
+    },
+    [onClose]
+  );
+
+  const handleCloseKeyPress = useCallback(
+    (event: KeyboardEvent): void => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      if (event.key !== "Enter" || !onClose) {
+        return;
+      }
+
+      onClose();
+    },
+    [onClose]
+  );
+
+  return (
+    <div className={classes}>
+      <div className={`${mainCssClass}__body`}>
+        <div className={`${mainCssClass}__close-wrapper`}>
+          <button
+            className={`${mainCssClass}__close`}
+            onClick={handleCloseClick}
+            onKeyPress={handleCloseKeyPress}
+          >
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+        </div>
+        <div className={`${mainCssClass}__error-text`}>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default memo(Error);
